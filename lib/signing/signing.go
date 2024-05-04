9bd84320839183d3a41740bcbf28649909c02ffb
Adam <f7f376a1fcd0d0e11a10ed1b6577c9@users.noreply.github.com>
2024-05-04 06:26:05 +0100

    fix: signing can now decode both hex and bech32

--- lib/signing/signing.go
+++ lib/signing/signing.go
@@ -8,16 +8,27 @@ import (
 
 	"github.com/btcsuite/btcd/btcec/v2"
 	"github.com/btcsuite/btcd/btcec/v2/schnorr"
+	"github.com/btcsuite/btcd/btcutil/bech32"
 	"github.com/decred/dcrd/dcrec/secp256k1/v4"
 	"github.com/ipfs/go-cid"
+	"github.com/libp2p/go-libp2p/core/crypto"
 )
 
-func DecodeKey(serializedKey string) ([]byte, error) {
-	fmt.Println(TrimPrivateKey(TrimPublicKey(serializedKey)))
+const PublicKeyPrefix = "npub1"
+const PrivateKeyPrefix = "nsec1"
 
+func DecodeKey(serializedKey string) ([]byte, error) {
 	bytes, err := hex.DecodeString(TrimPrivateKey(TrimPublicKey(serializedKey)))
 	if err != nil {
-		return nil, err
+		_, bytesToBits, err := bech32.Decode(serializedKey)
+		if err != nil {
+			return nil, fmt.Errorf("failed to decode key from hex or bech32: %v", err)
+		}
+
+		bytes, err = bech32.ConvertBits(bytesToBits, 5, 8, false)
+		if err != nil {
+			return nil, fmt.Errorf("failed to decode key from hex or bech32: %v", err)
+		}
 	}
 
 	return bytes, nil
@@ -49,11 +60,11 @@ func DeserializePublicKey(serializedKey string) (*secp256k1.PublicKey, error) {
 }
 
 func TrimPrivateKey(privateKey string) string {
-	return strings.TrimPrefix(privateKey, "nsec1")
+	return strings.TrimPrefix(privateKey, PrivateKeyPrefix)
 }
 
 func TrimPublicKey(publicKey string) string {
-	return strings.TrimPrefix(publicKey, "npub1")
+	return strings.TrimPrefix(publicKey, PublicKeyPrefix)
 }
 
 func SignData(data []byte, privateKey *btcec.PrivateKey) (*schnorr.Signature, error) {
@@ -108,6 +119,38 @@ func GeneratePrivateKey() (*secp256k1.PrivateKey, error) {
 func SerializePrivateKey(privateKey *secp256k1.PrivateKey) (*string, error) {
 	privateKeyBytes := privateKey.Serialize()
 
+	bytes, err := bech32.ConvertBits(privateKeyBytes, 8, 5, true)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode key from hex or bech32: %v", err)
+	}
+
+	encodedKey, err := bech32.Encode(PrivateKeyPrefix, bytes)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode key from hex or bech32: %v", err)
+	}
+
+	return &encodedKey, nil
+}
+
+func SerializePublicKeyBech32(publicKey *secp256k1.PublicKey) (*string, error) {
+	publicKeyBytes := schnorr.SerializePubKey(publicKey)
+
+	bytes, err := bech32.ConvertBits(publicKeyBytes, 8, 5, true)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode key from hex or bech32: %v", err)
+	}
+
+	encodedKey, err := bech32.Encode(PublicKeyPrefix, bytes)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode key from hex or bech32: %v", err)
+	}
+
+	return &encodedKey, nil
+}
+
+func SerializePrivateKeyBech32(privateKey *secp256k1.PrivateKey) (*string, error) {
+	privateKeyBytes := privateKey.Serialize()
+
 	encodedKey := hex.EncodeToString(privateKeyBytes)
 
 	return &encodedKey, nil
@@ -119,4 +162,37 @@ func SerializePublicKey(publicKey *secp256k1.PublicKey) (*string, error) {
 	encodedKey := hex.EncodeToString(publicKeyBytes)
 
 	return &encodedKey, nil
+}
+
+func ConvertPubKeyToLibp2pPubKey(publicKey *secp256k1.PublicKey) (*crypto.PubKey, error) {
+	serializedKey, err := SerializePublicKey(publicKey)
+	if err != nil {
+		return nil, err
+	}
+
+	decodedKey, err := DecodeKey(*serializedKey)
+	if err != nil {
+		return nil, err
+	}
+
+	secKey, err := schnorr.ParsePubKey(decodedKey)
+	if err != nil {
+		return nil, err
+	}
+
+	serializedPubKey := secKey.SerializeCompressed()
+
+	btcecPubKey, err := btcec.ParsePubKey(serializedPubKey)
+	if err != nil {
+		return nil, err
+	}
+
+	btcecPubKeyBytes := btcecPubKey.SerializeCompressed()
+
+	libp2pPublicKey, err := crypto.UnmarshalSecp256k1PublicKey(btcecPubKeyBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	return &libp2pPublicKey, nil
 }
