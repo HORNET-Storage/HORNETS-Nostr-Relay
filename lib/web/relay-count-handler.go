5fcb03ebc1c8a5191c53bb8c447da0966735b437
Maphikza <stapisi@protonmail.com>
2024-05-18 14:48:37 +0200

    Separating panels handlers

---
+++ lib/web/relay-count-handler.go
@@ -0,0 +1,100 @@
+package web
+
+import (
+	"log"
+
+	types "github.com/HORNET-Storage/hornet-storage/lib"
+	"github.com/gofiber/fiber/v2"
+	"github.com/spf13/viper"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func handleRelayCount(c *fiber.Ctx) error {
+	log.Println("Relay count request received")
+	// var json = jsoniter.ConfigCompatibleWithStandardLibrary
+
+	// Retrieve the database path from the config file using Viper
+	dbPath := viper.GetString("relay_stats_db")
+	if dbPath == "" {
+		log.Fatal("Database path not found in config")
+	}
+
+	// Initialize the Gorm database
+	db, err := gorm.Open(sqlite.Open(dbPath), &gorm.Config{})
+	if err != nil {
+		log.Printf("Failed to connect to the database: %v", err)
+		return c.Status(fiber.StatusInternalServerError).SendString("Internal Server Error")
+	}
+
+	// Retrieve relay settings from the config file using Viper
+	var relaySettings struct {
+		Kinds    []string `mapstructure:"kinds"`
+		Photos   []string `mapstructure:"photos"`
+		Videos   []string `mapstructure:"videos"`
+		GitNestr []string `mapstructure:"gitNestr"`
+	}
+	if err := viper.UnmarshalKey("relay_settings", &relaySettings); err != nil {
+		log.Fatalf("Error unmarshaling relay settings: %v", err)
+	}
+
+	// Initialize the response data
+	responseData := map[string]int{
+		"kinds":    0,
+		"photos":   0,
+		"videos":   0,
+		"gitNestr": 0,
+	}
+
+	// Aggregate counts for each category
+	responseData["kinds"], err = getKindCounts(db)
+	if err != nil {
+		log.Printf("Error getting kind counts: %v", err)
+		return c.Status(fiber.StatusInternalServerError).SendString("Error getting kind counts")
+	}
+
+	responseData["photos"], err = getPhotoCounts(db)
+	if err != nil {
+		log.Printf("Error getting photo counts: %v", err)
+		return c.Status(fiber.StatusInternalServerError).SendString("Error getting photo counts")
+	}
+
+	responseData["videos"], err = getVideoCounts(db)
+	if err != nil {
+		log.Printf("Error getting video counts: %v", err)
+		return c.Status(fiber.StatusInternalServerError).SendString("Error getting video counts")
+	}
+
+	responseData["gitNestr"], err = getGitNestrCounts(db, relaySettings.GitNestr)
+	if err != nil {
+		log.Printf("Error getting gitNestr counts: %v", err)
+		return c.Status(fiber.StatusInternalServerError).SendString("Error getting gitNestr counts")
+	}
+
+	log.Printf("Response Data: %+v", responseData)
+	return c.JSON(responseData)
+}
+
+func getKindCounts(db *gorm.DB) (int, error) {
+	var count int64
+	err := db.Model(&types.Kind{}).Count(&count).Error
+	return int(count), err
+}
+
+func getPhotoCounts(db *gorm.DB) (int, error) {
+	var count int64
+	err := db.Model(&types.Photo{}).Count(&count).Error
+	return int(count), err
+}
+
+func getVideoCounts(db *gorm.DB) (int, error) {
+	var count int64
+	err := db.Model(&types.Video{}).Count(&count).Error
+	return int(count), err
+}
+
+func getGitNestrCounts(db *gorm.DB, gitNestr []string) (int, error) {
+	var count int64
+	err := db.Model(&types.GitNestr{}).Where("git_type IN ?", gitNestr).Count(&count).Error
+	return int(count), err
+}
---
+++ lib/web/relay-settings-handler.go
@@ -0,0 +1,52 @@
+package web
+
+import (
+	"log"
+
+	types "github.com/HORNET-Storage/hornet-storage/lib"
+	"github.com/gofiber/fiber/v2"
+	jsoniter "github.com/json-iterator/go"
+	"github.com/spf13/viper"
+)
+
+func handleRelaySettings(c *fiber.Ctx) error {
+	log.Println("Relay settings request received")
+	var json = jsoniter.ConfigCompatibleWithStandardLibrary
+	var data map[string]interface{}
+	if err := c.BodyParser(&data); err != nil {
+		return c.Status(400).SendString(err.Error())
+	}
+
+	relaySettingsData, ok := data["relay_settings"]
+	if !ok {
+		log.Println("Relay settings data not provided")
+		return c.Status(400).SendString("Relay settings data expected")
+	}
+
+	var relaySettings types.RelaySettings
+	relaySettingsJSON, err := json.Marshal(relaySettingsData)
+	if err != nil {
+		log.Println("Error marshaling relay settings:", err)
+		return c.Status(fiber.StatusInternalServerError).SendString("Internal Server Error")
+	}
+
+	log.Println("Received relay settings JSON:", string(relaySettingsJSON))
+
+	if err := json.Unmarshal(relaySettingsJSON, &relaySettings); err != nil {
+		log.Println("Error unmarshaling relay settings:", err)
+		return c.Status(fiber.StatusInternalServerError).SendString("Internal Server Error")
+	}
+
+	// Store in Viper
+	viper.Set("relay_settings", relaySettings)
+
+	// Save the changes to the configuration file
+	if err := viper.WriteConfig(); err != nil {
+		log.Printf("Error writing config: %s", err)
+		return c.Status(fiber.StatusInternalServerError).SendString("Failed to update settings")
+	}
+
+	log.Println("Stored relay settings:", relaySettings)
+
+	return c.SendStatus(fiber.StatusOK)
+}
