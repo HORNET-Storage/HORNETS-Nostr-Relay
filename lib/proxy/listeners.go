60db54662b622442b6d82c791d7f289bf437f7d9
Maphikza <stapisi@protonmail.com>
2024-07-10 13:52:26 +0200

    Saving working basic nip-42 before making more changes

--- lib/proxy/listeners.go
+++ lib/proxy/listeners.go
@@ -2,7 +2,11 @@ package proxy
 
 import (
 	"context"
+	"crypto/rand"
+	"encoding/hex"
 	"fmt"
+
+	"sync/atomic"
 
 	"github.com/gofiber/contrib/websocket"
 	"github.com/nbd-wtf/go-nostr"
@@ -12,18 +16,23 @@ import (
 // Global map to hold all listeners indexed by WebSocket connections and subscription IDs.
 var listeners = xsync.NewMapOf[*websocket.Conn, ListenerData]()
 
+// Global challenge variable
+var globalChallenge atomic.Value
+
+const challengeLength = 32
+
 // SetListener sets a new listener with given ID, WebSocket connection, filters, and cancel function.
-func setListener(id string, ws *websocket.Conn, filters nostr.Filters, challenge *string, cancel context.CancelFunc) {
+func setListener(id string, ws *websocket.Conn, filters nostr.Filters, cancel context.CancelFunc) {
 	conData, _ := listeners.LoadOrCompute(ws, func() ListenerData {
 		return ListenerData{
-			challenge:     challenge,
+			challenge:     "",
 			subscriptions: xsync.NewMapOf[string, *Subscription](),
 		}
 	})
 
 	conData.subscriptions.Store(id, &Subscription{filters: filters, cancel: cancel})
-	conData.challenge = challenge
 	conData.authenticated = false
+	listeners.Store(ws, conData)
 }
 
 // RemoveListenerId removes a listener by its ID and cancels its context.
@@ -51,6 +60,9 @@ func removeListener(ws *websocket.Conn) {
 // NotifyListeners notifies all listeners with an event if it matches their filters.
 func notifyListeners(event *nostr.Event) {
 	listeners.Range(func(ws *websocket.Conn, conData ListenerData) bool {
+		if !conData.authenticated {
+			return true // Skip notification if not authenticated
+		}
 		conData.subscriptions.Range(func(id string, listener *Subscription) bool {
 			if !listener.filters.Match(event) {
 				return true
@@ -64,29 +76,13 @@ func notifyListeners(event *nostr.Event) {
 	})
 }
 
-// LogCurrentSubscriptions logs current subscriptions for debugging purposes.
-func logCurrentSubscriptions() {
-	empty := true // Assume initially that there are no subscriptions
-	listeners.Range(func(ws *websocket.Conn, conData ListenerData) bool {
-		conData.subscriptions.Range(func(id string, listener *Subscription) bool {
-			fmt.Printf("Subscription ID: %s, Filters: %+v\n", id, listener.filters)
-			empty = false // Found at least one subscription, so not empty
-			return true
-		})
-		return true
-	})
-	if empty {
-		fmt.Println("No active subscriptions.")
-	}
-}
-
 func GetListenerChallenge(ws *websocket.Conn) (*string, error) {
 	conData, ok := listeners.Load(ws)
 	if !ok {
 		return nil, fmt.Errorf("no listeners found for this WebSocket connection")
 	}
 
-	return conData.challenge, nil
+	return &conData.challenge, nil
 }
 
 func AuthenticateConnection(ws *websocket.Conn) error {
@@ -96,8 +92,24 @@ func AuthenticateConnection(ws *websocket.Conn) error {
 	}
 
 	conData.authenticated = true
-
 	listeners.Store(ws, conData)
 
 	return nil
+}
+
+// Generate the global challenge
+func generateGlobalChallenge() (string, error) {
+	bytes := make([]byte, challengeLength)
+	_, err := rand.Read(bytes)
+	if err != nil {
+		return "", fmt.Errorf("failed to generate random bytes: %v", err)
+	}
+	challenge := hex.EncodeToString(bytes)
+	globalChallenge.Store(challenge)
+	return challenge, nil
+}
+
+// Get the global challenge
+func getGlobalChallenge() string {
+	return globalChallenge.Load().(string)
 }
