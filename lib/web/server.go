e2404d331cb3cc3399426797da2217334edbef36
Maphikza <stapisi@protonmail.com>
2024-11-12 14:36:47 +0200

    feat: Add conditional kind 411 event creation on subscription tier update
    - Enhanced  function to conditionally create a new kind 411 event if there are changes in .
    - Passed  as a parameter to  and related route handlers.
    - Improved configuration update logic and comparison checks for  to minimize unnecessary kind 411 creation.

--- lib/web/server.go
+++ lib/web/server.go
@@ -68,7 +68,11 @@ func StartServer(store stores.Store) error {
 	secured.Get("/relaycount", func(c *fiber.Ctx) error {
 		return getRelayCount(c, store)
 	})
-	secured.Post("/relay-settings", updateRelaySettings)
+
+	secured.Post("/relay-settings", func(c *fiber.Ctx) error {
+		return updateRelaySettings(c, store)
+	})
+
 	secured.Get("/relay-settings", getRelaySettings)
 	secured.Get("/timeseries", func(c *fiber.Ctx) error {
 		return getProfilesTimeSeriesData(c, store)
