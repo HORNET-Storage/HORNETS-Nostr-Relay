bcac36d0adac18023495da56bf6c066c45cae153
Adam <f7f376a1fcd0d0e11a10ed1b6577c9@users.noreply.github.com>
2025-05-28 19:59:22 +0100

    fix: fixed accidental revert of imports for 2 files during last pr

--- test/dht_keys_test.go
+++ test/dht_keys_test.go
@@ -4,7 +4,7 @@ import (
 	"encoding/hex"
 	"testing"
 
-	"github.com/HORNET-Storage/hornet-storage/lib/signing"
+	"github.com/HORNET-Storage/go-hornet-storage-lib/lib/signing"
 	"github.com/HORNET-Storage/hornet-storage/lib/sync"
 	"github.com/btcsuite/btcd/btcec/v2"
 	"github.com/btcsuite/btcd/btcec/v2/schnorr"
