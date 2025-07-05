b8d313a0c7a00d373519def49cb3d3fb31bd496c
Adam <f7f376a1fcd0d0e11a10ed1b6577c9@users.noreply.github.com>
2025-07-05 21:48:11 +0100

    fix: default paths not relative

--- lib/config/config.go
+++ lib/config/config.go
@@ -59,7 +59,7 @@ func setDefaults() {
 	viper.SetDefault("server.hornets", true)
 	viper.SetDefault("server.web", true)
 	viper.SetDefault("server.demo", false)
-	viper.SetDefault("server.data_path", "/data")
+	viper.SetDefault("server.data_path", "./data")
 	viper.SetDefault("server.stats_db", "statistics.db")
 
 	// External services defaults
@@ -74,7 +74,7 @@ func setDefaults() {
 	// Logging defaults
 	viper.SetDefault("logging.level", "info")
 	viper.SetDefault("logging.output", "both")
-	viper.SetDefault("logging.path", "/logs")
+	viper.SetDefault("logging.path", "./logs")
 
 	// Relay defaults
 	viper.SetDefault("relay.name", "HORNETS")
